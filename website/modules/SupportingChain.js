2120151787760cee1aa9bf80106850b8315d3c7f
akerckho <akerckho@ulb.be>
2021-11-26 13:08:31 +0100

    Markus scares me

--- website/modules/SupportingChain.js
+++ website/modules/SupportingChain.js
@@ -8,6 +8,7 @@ class SupportingChain{
         this.C_i = zoo.cages[index]
         this.index = index
         this.chains = this.getSupportingChains(index)
+        this.contact_points = this.getContactPoint()
     }
     /**
      * Computes and return the two supporting chain of cage C_i, respectively 
@@ -36,18 +37,44 @@ class SupportingChain{
         let supporting_chains = []
 
         let prev_chain = new Funnel(this.poly)
-        prev_chain.addPoint(this.C_i.getStartPoint())
-        prev_chain.addPoint(after === this.zoo.chair ? this.zoo.chair : after.getEndPoint())
+        prev_chain.addPoint(previous === this.zoo.chair ? this.zoo.chair : previous.getStartPoint())
+        prev_chain.addPoint(this.C_i.getEndPoint())
         prev_chain.funnel()
         supporting_chains.push(prev_chain.path)
 
         let after_chain = new Funnel(this.poly)
-        after_chain.addPoint(previous === this.zoo.chair ? this.zoo.chair : previous.getStartPoint())
-        after_chain.addPoint(this.C_i.getEndPoint())
+        after_chain.addPoint(this.C_i.getStartPoint())
+        after_chain.addPoint(after === this.zoo.chair ? this.zoo.chair : after.getEndPoint())
         after_chain.funnel()
         supporting_chains.push(after_chain.path)
+
+        
         
         return supporting_chains
+    }
+
+    getContactPoint() {
+        let chain1 = this.chains[0]  // Ci-1 -> Ci
+        let chain2 = this.chains[1]  // Ci -> Ci+1
+        
+        let contact_points = []
+        for (let i=0; i< chain1.length; i++){
+            if (this.C_i.points.includes(chain1[i])) {
+                contact_points.push(chain1[i])
+                contact_points[0].label = "b"
+                console.log(contact_points[0])
+                break
+            }
+        }
+        for (let i=1; i< chain2.length; i++){
+            if (!this.C_i.points.includes(chain2[i])) {
+                contact_points.push(chain2[i-1])
+                contact_points[1].label = "a"
+                console.log(contact_points[1])
+                break
+            }
+        }
+        return contact_points
     }
 
     draw(){
@@ -56,6 +83,12 @@ class SupportingChain{
             for (let j=0; j<this.chains[i].length-1; j++){
                 drawSegment(this.chains[i][j], this.chains[i][j+1], colors[i])
             }
+        }
+
+        fill("red");
+        for (let i=0; i < this.contact_points.length; i++){
+            ellipse(this.contact_points[i].x, this.contact_points[i].y, 4, 4);
+            text(this.contact_points[i].label, this.contact_points[i].x, this.contact_points[i].y);
         }
     }
 
