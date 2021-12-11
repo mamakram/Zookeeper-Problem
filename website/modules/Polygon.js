5b27ebd725a4f534fe6fc64ca6a6722a970b035a
mamakram <markus.makram@hotmail.com>
2021-12-11 15:22:24 +0100

    small bug fix

--- website/modules/Polygon.js
+++ website/modules/Polygon.js
@@ -216,10 +216,10 @@ class Triangle extends Polygon {
     let p2 = this.points[1];
     let p3 = this.points[2];
     return (
-      (!isRT(p1, p2, p, 100) &&
-        !isRT(p2, p3, p, 100) &&
-        !isRT(p3, p1, p, 100)) ||
-      (!isLT(p1, p2, p, 100) && !isLT(p2, p3, p, 100) && !isLT(p3, p1, p, 100))
+      (!isRT(p1, p2, p, 200) &&
+        !isRT(p2, p3, p, 200) &&
+        !isRT(p3, p1, p, 200)) ||
+      (!isLT(p1, p2, p, 200) && !isLT(p2, p3, p, 200) && !isLT(p3, p1, p, 200))
     );
   }
 
